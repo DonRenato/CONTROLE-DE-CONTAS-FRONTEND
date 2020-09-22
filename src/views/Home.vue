4e6cfa9fbea64682fb524fbf7e7fc6fd2ba1f682
Renato  Avellar <onizuka@Mac-mini.local>
2020-09-22 16:11:27 -0300

    criando estrutura da aplicação

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Hello World"/>
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
