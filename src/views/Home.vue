b5479549ffd3c38ae74e8624b3b301a48fc88ec6
Renato  Avellar <onizuka@Mac-mini.local>
2020-09-25 10:09:53 -0300

    criando home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <v-row justify="center">
+      <h1>Bem vindo(a) ao Controle de Contas da NYD</h1>
+    </v-row>
+
+    <v-row justify="center">
+      <v-img
+        src="https://www.imagemhost.com.br/images/2020/09/25/logo-nyd.png"
+        contain
+        aspect-ratio="1.7"
+        max-width="500px"
+        >
+      </v-img>
+    </v-row>
+
+    
   </div>
 </template>
 
